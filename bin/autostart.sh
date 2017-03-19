f4126248ef3b47712ff5daf710d7ad68c717acf2
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2017-03-19 23:01:31 +0900

    update autostart

--- bin/autostart.sh
+++ bin/autostart.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+## /etc/supervisor/conf.d/piano.conf
+# [program:piano]
+# command=/home/emile/projects/piano.node/bin/autostart.sh
+# directory=/home/emile/projects/piano.node
+# stdout_logfile_maxbytes=1MB
+# stderr_logfile_maxbytes=1MB
+# stdout_logfile_backups=5
+# stderr_logfile_backups=5
+# stdout_logfile=/home/emile/projects/piano.node/log/%(program_name)s.log
+# stderr_logfile=/home/emile/projects/piano.node/log/%(program_name)s_error.log
+# autorestart=false
+# autostart=true
+# user=emile
+
 # Run fluidsynth, but this time as a non-interactive server
 fluidsynth --audio-driver=alsa -o audio.alsa.device=hw:2 /usr/share/sounds/sf2/FluidR3_GM.sf2
 # fluidsynth -is --audio-driver=alsa --gain 3 /usr/share/sounds/sf2/FluidR3_GM.sf2 &
@@ -12,7 +26,7 @@ sleep 10
 aconnect 16:0 128:0
 
 # Give fluidsynth a nice high priority so it gets as much CPU as possible!
-sudo renice -n -18 -u synth
+# sudo renice -n -18 -u synth
 
 # give it time
 sleep 3
