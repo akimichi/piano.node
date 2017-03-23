69073a79c774541e94e5a357f677f9e5c537c44d
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2017-03-23 20:59:24 +0900

    fix autostart.sh

--- bin/autostart.sh
+++ bin/autostart.sh
@@ -14,16 +14,41 @@
 # autostart=true
 # user=emile
 
+# Launch vmpk and connect it to fluidsynth.
+# Based on a script by Antonio Bonifati.
+
+# If vmpk isn't running
+# if ! pgrep "^vmpk$"
+# then
+#     # Launch vmpk
+#     vmpk &
+#     # Wait for it to come up.
+#     sleep 1
+# fi
+
+# # Use aconnect to get the ports
+# vmpkport=$(aconnect -i | grep "client.*VMPK Output" | cut -d ' ' -f 2)0
+# synthport=$(aconnect -o | grep "FLUID Synth" | cut -d ' ' -f 2)0
+
+# echo vmpk output port: $vmpkport
+# echo fluidsynth input port: $synthport
+
+# # Connect the ports
+# aconnect $vmpkport $synthport
+
+
 # Run fluidsynth, but this time as a non-interactive server
-fluidsynth --audio-driver=alsa -o audio.alsa.device=hw:2 /usr/share/sounds/sf2/FluidR3_GM.sf2
-# fluidsynth -is --audio-driver=alsa --gain 3 /usr/share/sounds/sf2/FluidR3_GM.sf2 &
+# fluidsynth --audio-driver=alsa -o audio.alsa.device=hw:0 /usr/share/sounds/sf2/FluidR3_GM.sf2 
+# fluidsynth --audio-driver=alsa -o audio.alsa.device=hw:2 /usr/share/sounds/sf2/FluidR3_GM.sf2
+fluidsynth -is --audio-driver=alsa --gain 3 /usr/share/sounds/sf2/FluidR3_GM.sf2 &
 
 # give it time to boot up
-sleep 10
+sleep 30
 
 # connect the controller to fluidsynth
 # Don't forget to replace these with the client numbers!
-aconnect 16:0 128:0
+# while ! aconnect 20:0 128:0; do sleep 1; done
+aconnect 20:0 128:0
 
 # Give fluidsynth a nice high priority so it gets as much CPU as possible!
 # sudo renice -n -18 -u synth
